f72ee05768cc22e6f0f8d9ec24b8008286747342
PabloNoval88 <pablons77@educastur.es>
2025-10-28 12:39:56 +0100

    adgsdgsdf

---
+++ Noval_Pablo_P5JS_TDII/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+  background(100,255,255);
+
+  // Línea mínima
+  line(0, 0, 10, 10);
+
+  // Color de trazo (RGB)
+  stroke(255, 0, 0);
+  line(0, 0, 10, 10);
+
+  stroke(0, 255, 0);
+  strokeWeight(10);
+  line(200, 100, 150, 300);
+  line(200,100,250,300);
+  line(180,220,220,220);
+}
