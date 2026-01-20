beae3f7c38f19667a311042e44e9f573b53c208a
PabloNoval88 <pablons77@educastur.es>
2026-01-20 12:21:18 +0100

    Add files via upload

---
+++ Noval_Pablo_P5JS_TDII/sketch1.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(400, 400); 
+  background(255);        
+
+  
+  stroke(255, 0, 124,);      
+  strokeWeight(25);        
+  line(100, 100, 300, 100);
+
+  
+  stroke(22,187, 0,100); 
+  strokeWeight(50);        
+  line(300, 100, 300, 300);
+
+  
+  stroke(0, 0, 255);      
+  strokeWeight(36);        
+  line(300, 300, 100, 300);
+
+  
+  stroke(120,100,0,230);            
+  strokeWeight(44);        
+  line(100, 300, 100, 100);
+}
---
+++ Noval_Pablo_P5JS_TDII/sketch2.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(400, 400);    
+  background(255);           
+
+  strokeweight(4);
+  fill(255,255,255);
+  rect(150, 250, 100, 60);
+
+  
+  strokeweight(4);
+  fill(0, 0, 255);          
+  rect(200, 50, 100, 60);
+
+  stroke(86,44,6);
+  strokeweight(4);
+  fill(255, 0, 0, 120);      
+  rect(50, 150, 100, 60);
+
+ strokeweight(4);
+  fill(0, 200, 80);
+  rect(200, 150, 100, 60);
+
+ strokeweight(4);
+  fill(0,0,255,30);
+  triangle(0,400,400,0,400,400);
+}
